aa369ef43a5d252a650210f0eaa1ed60c6fc56c9
Lindsay W <lwhite5314@gmail.com>
2020-05-13 15:01:45 -0400

    mostly working

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,51 +1,92 @@
 import React, { useState } from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup.string().email("Must be a valid email address").required("Must include email address"),
+    password: yup.string().required("Must include password"),
+    terms: yup.boolean().oneOf([true],"Please agree to terms of use")
+})
 
 
-const Form = props => {
-    const[form, setForm]=useState({
+export default function Form (){
+    const[formState, setFormState]=useState({
         id:Date.now(),
         name:"",
         email:"",
         password:"",
         terms: false
+    });
 
-    })
+    const[errorState, setErrorState]=useState({
+        id:Date.now(),
+        name:"",
+        email:"",
+        password:"",
+        terms: ""
+    });
 
-    const changeHandler = (event)=>{
-        setForm({
-            ...form,[event.target.name]:event.target.value
+    const validate = e =>{
+        let value = e.target.type==="checkbox"? e.target.checked:e.target.value;
+        yup.reach(formSchema, e.target.name)
+        .validate(value)
+        .then(valid=>{
+            setErrorState({
+                ...errorState, [e.target.name]: ""    
+            });
+        })
+        .catch(err=>{
+            setErrorState({
+                ...errorState,[e.target.name]:err.errors[0]
+            });
+        });
+        };
+
+    const changeHandler = (e)=>{
+        e.persist();
+        validate(e);
+        let value = e.target.type ==="checkbox"? e.target.checked:e.target.value;
+        setFormState({
+            ...formState,[e.target.name]:value
         })
     }
 
+    const formSubmit = e =>{
+        e.preventDefault();
+        console.log("form submitted!");
+        axios.post("https://regres.in/api/users", formState)
+            .then(response=>console.log(response))
+            .catch(err=> console.log(err));
+    };
+
     return (
         <div>
-        <form onSubmit={event=>{ 
-            event.preventDefault(); 
-            console.log(form);
-            setForm({name:"", email:"", password:"", terms: false, id:Date.now()})
-        }}>
-            <label htmlFor="name">Name:</label>
-            <input id="name" type="text" name="name" placeholder="Please enter name" value={form.name}
+        <form onSubmit={formSubmit}>
+            <label htmlFor="name">Name:
+            <input id="name" type="text" name="name" placeholder="Please enter name" value={formState.name}
                 onChange={changeHandler} />
+             </label>
 
+            <label htmlFor="email">Email:
+            <input id="email" type="text" name="email" placeholder="Please enter email" value={formState.email} onChange={changeHandler} />
+            {errorState.email.length>0?(
+                <p className="error">{errorState.email}</p>
+            ) :null}
+            </label>
 
-            <label htmlFor="email">Email:</label>
-            <input id="email" type="text" name="email" placeholder="Please enter email" value={form.email}
+            <label htmlFor="password">Password:
+            <input id="password" type="text" name="password" placeholder="Please enter password" value={formState.password}
                 onChange={changeHandler} />
+            </label>
 
-            <label htmlFor="password">Password:</label>
-            <input id="password" type="text" name="password" placeholder="Please enter password" value={form.password}
-                onChange={changeHandler} />
-
-
-            <label htmlFor="terms">Terms of Service</label>
-            <input id="terms" type="checkbox" checked={form.terms} onChange={changeHandler} />
-
+            <label htmlFor="terms">Terms of Service
+            <input id="terms" type="checkbox" checked={formState.terms} onChange={changeHandler} />
+            </label>
             <button type="submit">Submit</button>
         </form>
        </div>
     )
 
 }
-export default Form;
 
